025d074399ee504892e4acceb1873cc43fd999be
iniestano8 <iniestano8@163.com>
2015-10-20 08:32:57 +0800

    some util & spring interceptor config

---
+++ dbdou-util/src/main/java/com/dbdou/util/common/SecurityUtil.java
@@ -0,0 +1,57 @@
+package com.dbdou.util.common;
+
+import java.security.MessageDigest;
+
+public class SecurityUtil {
+
+	public final static String MD5(String s) {
+		char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+				'A', 'B', 'C', 'D', 'E', 'F' };
+		try {
+			byte[] btInput = s.getBytes();
+			// 获得MD5摘要算法的 MessageDigest 对象
+			MessageDigest mdInst = MessageDigest.getInstance("MD5");
+			// 使用指定的字节更新摘要
+			mdInst.update(btInput);
+			// 获得密文
+			byte[] md = mdInst.digest();
+			// 把密文转换成十六进制的字符串形式
+			int j = md.length;
+			char str[] = new char[j * 2];
+			int k = 0;
+			for (int i = 0; i < j; i++) {
+				byte byte0 = md[i];
+				str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+				str[k++] = hexDigits[byte0 & 0xf];
+			}
+			return new String(str);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * 采用md5加密，与主流md5算法库不一样，主流暴库工具均无法免费破解该加密数据。
+	 */
+	public static String md5(String src) {
+		try {
+			StringBuffer buffer = new StringBuffer();
+			char[] chars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+					'A', 'B', 'C', 'D', 'E', 'F' };
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			byte[] data = md.digest(src.getBytes());
+			for (byte b : data) {
+				// 高4位
+				buffer.append(chars[(b >> 4) & 0x0F]);
+				// 低4位
+				buffer.append(chars[b & 0x0F]);
+			}
+			return buffer.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
